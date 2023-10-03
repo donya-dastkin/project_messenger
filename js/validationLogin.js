d58848e9d831de9cea76d44c06704b503a1f0b2a
Donya Dastkin <donya.dastkin583@gmail.com>
2023-10-04 00:21:43 +0330

    Completion of source login and signup

--- js/validationLogin.js
+++ js/validationLogin.js
@@ -1,161 +1,176 @@
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const container = document.getElementById("container");
+
+signUpButton.addEventListener("click", () =>
+  container.classList.add("right-panel-active")
+);
+
+signInButton.addEventListener("click", () =>
+  container.classList.remove("right-panel-active")
+);
+
 const signupBtn = document.getElementById("signupBtn");
 const loginBtn = document.getElementById("loginBtn");
-let ErrorMessageS=document.getElementById("ErrorMessageSignup");
-let ErrorMessageL=document.getElementById("ErrorMessageLogin");
 
 const ValidationSignup = () => {
+  let ErrorMessageS = document.getElementById("ErrorMessageSignup");
+  const phone = document.forms["Signup"]["phone"].value;
+  const password = document.forms["Signup"]["password"].value;
+  const repassword = document.forms["Signup"]["repassword"].value;
 
-const phone = document.forms["Signup"]["phone"].value;
-const password = document.forms["Signup"]["password"].value;
-const repassword = document.forms["Signup"]["repassword"].value;
-
-const validationPhone = () => {
-    let valid = false
-    if (phone.length === 11 && phone.startsWith('09')) {
-        valid = true
+  const validationPhone = () => {
+    let valid = false;
+    if (phone.length === 11 && phone.startsWith("09")) {
+      valid = true;
     } else {
-        valid = false
+      valid = false;
     }
-    return valid
-}
+    return valid;
+  };
 
-const validationPasswordLength = () => {
-    let valid = false
+  const validationPasswordLength = () => {
+    let valid = false;
     if (password.length >= 8 && repassword.length >= 8) {
-        valid = true
+      valid = true;
     } else {
-        valid = false
+      valid = false;
     }
-    return valid
-}
+    return valid;
+  };
 
-const validationPassword = () => {
-    let valid = false
+  const validationPassword = () => {
+    let valid = false;
     if (password === repassword) {
-        valid = true
+      valid = true;
     } else {
-        valid = false
+      valid = false;
     }
-    return valid
-}
+    return valid;
+  };
 
-const validationFilde = () => {
-    let valid = false
+  const validationFilde = () => {
+    let valid = false;
     if (phone === "" && password === "" && repassword === "") {
-        valid = false
+      valid = false;
     } else if (phone !== "" && password !== "" && repassword !== "") {
-        valid = true
+      valid = true;
     }
-    return valid
-}
+    return valid;
+  };
 
-const Phone = validationPhone()
-const PasswordLength = validationPasswordLength()
-const Password = validationPassword()
-const Filde = validationFilde()
+  const Phone = validationPhone();
+  const PasswordLength = validationPasswordLength();
+  const Password = validationPassword();
+  const Filde = validationFilde();
 
-if(Phone === false){
-    ErrorMessageS.textContent="شماره موبایل را به درستی وارد کنید!!"
-}
+  if (Phone === false) {
+    ErrorMessageS.textContent = "شماره موبایل را به درستی وارد کنید!!";
+  }
 
-if(PasswordLength === false){
-    ErrorMessageS.textContent="تعداد ارقام های پسورد باید 8 رقم باشد"
-}
+  if (PasswordLength === false) {
+    ErrorMessageS.textContent = "تعداد ارقام های پسورد باید 8 رقم باشد";
+  }
 
-if(Password === false){
-    ErrorMessageS.textContent="پسورد و تکرار ان با یک دیگر مطابقت ندارند!!"
-}
+  if (Password === false) {
+    ErrorMessageS.textContent = "پسورد و تکرار ان با یک دیگر مطابقت ندارند!!";
+  }
 
-if(Filde === false){
-    ErrorMessageS.textContent="لطفا فیلد را پرکنید!!"
-}
+  if (Filde === false) {
+    ErrorMessageS.textContent = "لطفا فیلد را پرکنید!!";
+  }
 
-if (Phone === false && PasswordLength === false && Password === false && Filde === false) {
+  if (
+    Phone === false &&
+    PasswordLength === false &&
+    Password === false &&
+    Filde === false
+  ) {
     signupBtn.disabled = false;
     signupBtn.setAttribute("class", "submit_disabled");
-} else if(Phone !== false && PasswordLength !== false && Password !== false && Filde !== false) {
-    ErrorMessageS.textContent=""
+  } else if (
+    Phone !== false &&
+    PasswordLength !== false &&
+    Password !== false &&
+    Filde !== false
+  ) {
+    ErrorMessageS.textContent = "";
     signupBtn.disabled = true;
     signupBtn.setAttribute("class", "submit");
-}
-
-}
+  }
+};
 
 const ValidationLogin = () => {
+  let ErrorMessageL = document.getElementById("ErrorMessageLogin");
+  const phone = document.forms["Login"]["phone"].value;
+  const password = document.forms["Login"]["password"].value;
 
-    const phone = document.forms["Login"]["phone"].value;
-    const password = document.forms["Login"]["password"].value;
-    
-    const validationPhone = () => {
-        let valid = false
-        if (phone.length === 11 && phone.startsWith('09')) {
-            valid = true
-        } else {
-            valid = false
-        }
-        return valid
+  const validationPhone = () => {
+    let valid = false;
+    if (phone.length === 11 && phone.startsWith("09")) {
+      valid = true;
+    } else {
+      valid = false;
     }
-    
-    const validationPasswordLength = () => {
-        let valid = false
-        if (password.length >= 8) {
-            valid = true
-        } else {
-            valid = false
-        }
-        return valid
-    }
-    
-    // const validationPassword = () => {
-    //     let valid = false
-    //     if (password === repassword) {
-    //         valid = true
-    //     } else {
-    //         valid = false
-    //     }
-    //     return valid
-    // }
-    
-    const validationFilde = () => {
-        let valid = false
-        if (phone === "" && password === "") {
-            valid = false
-        } else if (phone !== "" && password !== "") {
-            valid = true
-        }
-        return valid
-    }
-    
-    const Phone = validationPhone()
-    const PasswordLength = validationPasswordLength()
-    // const Password = validationPassword()
-    const Filde = validationFilde()
-    if(Phone === false){
+    return valid;
+  };
 
-        ErrorMessageL.textContent="شماره موبایل را به درستی وارد کنید!!"
+  const validationPasswordLength = () => {
+    let valid = false;
+    if (password.length >= 8) {
+      valid = true;
+    } else {
+      valid = false;
     }
+    return valid;
+  };
 
-    if(PasswordLength === false){
+  // const validationPassword = () => {
+  //     let valid = false
+  //     if (password === repassword) {
+  //         valid = true
+  //     } else {
+  //         valid = false
+  //     }
+  //     return valid
+  // }
 
-        ErrorMessageL.textContent="تعداد ارقام های پسورد باید 8 رقم باشد"
+  const validationFilde = () => {
+    let valid = false;
+    if (phone === "" && password === "") {
+      valid = false;
+    } else if (phone !== "" && password !== "") {
+      valid = true;
     }
-    
-    if(Filde === false){
+    return valid;
+  };
 
-        ErrorMessageL.textContent="لطفا فیلد را پرکنید!!"
-    }
+  const Phone = validationPhone();
+  const PasswordLength = validationPasswordLength();
+  // const Password = validationPassword()
+  const Filde = validationFilde();
+  if (Phone === false) {
+    ErrorMessageL.textContent = "شماره موبایل را به درستی وارد کنید!!";
+  }
 
-    if (Phone === false && PasswordLength === false && Filde === false) {
-        loginBtn.disabled = false;
-        loginBtn.setAttribute("class", "submit_disabled");
-    } else if(Phone !== false && PasswordLength !== false && Filde !== false) {
-        ErrorMessageL.textContent=""
-        loginBtn.disabled = true;
-        loginBtn.setAttribute("class", "submit");
-    }
-    
-    }
+  if (PasswordLength === false) {
+    ErrorMessageL.textContent = "تعداد ارقام های پسورد باید 8 رقم باشد";
+  }
 
-const SubmitForm=()=>{
-    document.getElementById("Signup").submit();
-}
+  if (Filde === false) {
+    ErrorMessageL.textContent = "لطفا فیلد را پرکنید!!";
+  }
+
+  if (Phone === false && PasswordLength === false && Filde === false) {
+    loginBtn.disabled = false;
+    loginBtn.setAttribute("class", "submit_disabled");
+  } else if (Phone !== false && PasswordLength !== false && Filde !== false) {
+    ErrorMessageL.textContent = "";
+    loginBtn.disabled = true;
+    loginBtn.setAttribute("class", "submit");
+  }
+};
+
+// const SubmitForm=()=>{
+//     document.getElementById("Signup").submit();
+// }

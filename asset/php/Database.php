709a47d17ad84e60365705c981ae1c56ad7f8969
Donya Dastkin <donya.dastkin583@gmail.com>
2023-12-29 10:32:00 +0330

    Selecting chat messages Part2

---
+++ asset/php/Database.php
@@ -0,0 +1,59 @@
+<?php
+
+//? function
+
+function connect($servername, $username, $password, $dbname){
+    
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    
+    return $conn;
+
+}
+
+function sendTime(){
+    
+    date_default_timezone_set("Asia/Tehran");
+    $time = time();
+    return date("h:i:sa", $time);
+
+}
+
+function insertData($conn, $data,$table){
+    $sendTime=sendTime();
+    $sql = "INSERT INTO $table (messagetext,sendertype,sendtime,chatname) 
+        VALUES ('$data',0,'$sendTime','Donya Dstkin')";
+        if ($conn->query($sql) === TRUE) {
+            echo "\n New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+}
+
+function selectAllData($conn,$table){
+
+    $sql = "SELECT * FROM `$table` ORDER BY `id` ASC";
+    $result = mysqli_query($conn,$sql);
+    
+    $data = array("id"=>"", "messagetext"=>"", "sendertype"=>"","sendtime"=>"","chatname"=>"");
+    
+    while($row = mysqli_fetch_array($result)){
+    
+    $data["id"]=$row["id"];
+    $data["messagetext"]=$row["messagetext"];
+    $data["sendertype"]=$row["sendertype"];
+    $data["sendtime"]=$row["sendtime"];
+    $data["chatname"]=$row["chatname"];
+    
+    $json = json_encode($data);
+    
+    echo $json;
+    echo "<br>";
+    echo "<br>";
+    
+    };
+    
+    }
--- asset/php/index.php
+++ asset/php/index.php
@@ -1,48 +1,20 @@
 <?php
 
-$message = $_GET['dialog__message'];
-echo $message;
+include 'Database.php' ;
 
-//? function
-
-function connect($servername, $username, $password, $dbname){
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-return $conn;
-
-}
-
-function insertData($conn, $data,$table){
-    
-$sql = "INSERT INTO $table (messagetext) VALUES ('$data')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "\n New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-}
-
-function selectAllData($conn,$table){
-$sql = "SELECT * FROM $table";
-
-if ($conn->query($sql) !== TRUE) {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-}
+$message ="";
+//  $_GET['dialog__message'];
 
 //? run function
 
 $conn = connect("localhost", "root", "", "chat");
 $table = "message";
-insertData($conn, $message,$table);
+
+if ($message!== "") {
+    insertData($conn, $message, $table);
+}
+
+selectAllData($conn,$table);
 
 $conn->close();
 ?>

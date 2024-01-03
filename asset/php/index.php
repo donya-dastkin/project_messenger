26324bc6829a33b4247180743fb23bc2771d5110
Mostafa <147512413+MostafaMahdavipoor@users.noreply.github.com>
2024-01-03 20:00:14 +0330

    Update index.php

---
+++ asset/php/index.php
@@ -0,0 +1,12 @@
+<?php
+
+include 'connection.php';
+
+$message = $_GET['dialog__message'];
+$message = strip_tags(trim($message));
+
+if (!empty($message)) {
+    insertData($message);
+}
+
+R::close();

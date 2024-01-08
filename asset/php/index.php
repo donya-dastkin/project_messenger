a7fb21877735338b7e4be1d447cd50dea4202fc3
Fateme Zaka <ftmzaka.pr02@gmail.com>
2024-01-08 15:06:13 +0330

    add update message method

--- asset/php/index.php
+++ asset/php/index.php
@@ -3,12 +3,14 @@
 include 'connection.php';
 
 $message = $_GET['dialog__message'];
+$chat_name=$_GET['activeChatlist'];
+
 
 $message = strip_tags(trim($message));
 
 
 if (!empty($message)) {
-    insertData($message, 191);
+    insertData($message, 191,$chat_name);
 }
 
 R::close();

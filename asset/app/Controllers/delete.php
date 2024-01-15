3bc0a5bd6c034f5c2e1762f3f93d1f4082c5425d
Fateme Zaka <ftmzaka.pr02@gmail.com>
2024-01-15 18:18:35 +0330

    development of delete method

--- asset/app/Controllers/delete.php
+++ asset/app/Controllers/delete.php
@@ -2,24 +2,69 @@
 
 require '../Models/messages/Message.php';
 
-$id = $_GET['dataID'];
-$id = $id;
 
-if (!empty($id)) {
-    $message = new Message();
-    try {
-        $message->deleteData([$id]);
-        header('Content-Type: application/json');
-        http_response_code(200);
-        echo json_encode([
-            'status' => 'success',
-            'message' => 'deleted...',
-        ]);
-    } catch (Exception $err) {
-        header('Content-Type: application/json');
-        http_response_code(500);
-        error_log($err->getMessage()."\n", 3, "err.txt");
-    }
+$deleteType = $_GET['deleteType'];
+$message = new Message();
+
+switch ($deleteType) {
+    case 'single-real': {
+            $id = $_GET['dataID'];
+            if (!empty($id)) {
+                try {
+                    $message->deleteData([$id]);
+                    header('Content-Type: application/json');
+                    http_response_code(200);
+                    echo json_encode([
+                        'status' => 'success',
+                        'message' => 'deleted...',
+                    ]);
+                } catch (Exception $err) {
+                    header('Content-Type: application/json');
+                    http_response_code(500);
+                    error_log($err->getMessage() . "\n", 3, "err.txt");
+                }
+            }
+            break;
+        }
+    case 'single-physical': {
+            $id = $_GET['dataID'];
+            if (!empty($id)) {
+                try {
+                    $message->deleteDataphysical($id);
+                    header('Content-Type: application/json');
+                    http_response_code(200);
+                    echo json_encode([
+                        'status' => 'success',
+                        'message' => 'deleted...',
+                    ]);
+                } catch (Exception $err) {
+                    header('Content-Type: application/json');
+                    http_response_code(500);
+                    error_log($err->getMessage() . "\n", 3, "err.txt");
+                }
+            }
+            break;
+        }
+    case 'integrated': {
+            $chatlistName = $_GET['activeChatlist'];
+            if (!empty($chatlistName)) {
+                try {
+                    $id = $message->deleteChatHistory($chatlistName);
+                    header('Content-Type: application/json');
+                    http_response_code(200);
+                    echo json_encode([
+                        'status' => 'success',
+                        'message' => 'deleted...',
+                        'data' => $id,
+                    ]);
+                } catch (Exception $err) {
+                    header('Content-Type: application/json');
+                    http_response_code(500);
+                    error_log($err->getMessage() . "\n", 3, "err.txt");
+                }
+            }
+            break;
+        }
 }
 
 R::close();

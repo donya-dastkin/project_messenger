0a5c58f69d07251a5535cfb626386de3ad0cabb6
Fateme Zaka <ftmzakapr02@gmail.com>
2024-02-01 10:36:38 +0330

    feat:autoloading by composer

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
-require '/xampp/htdocs/project_messanger/asset/app/Router/Router.php';
+require '/xampp/htdocs/project_messanger/vendor/autoload.php';
+use project\Router\Router;
 $requestURI =$_SERVER['REQUEST_URI'] ;
 
 $obj = new Router($requestURI);
 $obj->routing();
+
